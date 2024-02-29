146d17d5e140f7aab663c12a6ff4f5b9d99968e3
gary <garyliggins@yahoo.com>
2024-02-28 21:43:09 -0800

    completed, my code works but 2 tests are not passing

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main = document.getElementById("main")
+main.remove();
+
+const newHeader = document.createElement("h1")
+document.body.append(newHeader)
+newHeader.id = "victory"
+// newHeader.setAttribute("id", 'victory')
+newHeader.textContent = "Gary is the champion"
